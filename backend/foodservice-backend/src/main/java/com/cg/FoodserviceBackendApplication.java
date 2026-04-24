2760dd6d07c549de9c203c38029953918c675549
Arpit Bisht <arpitbisht.31415@gmail.com>
2026-04-24 15:38:46 +0530

    Added the Customer Entity class

---
+++ backend/foodservice-backend/src/main/java/com/cg/FoodserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodserviceBackendApplication.class, args);
+	}
+
+}
