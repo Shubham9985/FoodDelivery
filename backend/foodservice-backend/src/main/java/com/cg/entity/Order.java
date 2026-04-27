1bba9a5f0c2eccb883979e8b0e29b4bcbf283428
Anima <animasingh78@gmail.com>
2026-04-27 11:59:35 +0530

    modified order and orderitem

--- backend/foodservice-backend/src/main/java/com/cg/entity/Order.java
+++ backend/foodservice-backend/src/main/java/com/cg/entity/Order.java
@@ -1,73 +1,68 @@
 package com.cg.entity;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
 
 @Entity
 public class Order {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
-    
+
     private LocalDateTime orderDate;
-    
+
     private String orderStatus;
-    
+
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
-    
+
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
-    
+
     @ManyToOne
     @JoinColumn(name = "delivery_driver_id")
     private DeliveryDriver deliveryDriver;
-	public Integer getOrderId() {
-		return orderId;
-	}
-	public void setOrderId(Integer orderId) {
-		this.orderId = orderId;
-	}
-	public LocalDateTime getOrderDate() {
-		return orderDate;
-	}
-	public void setOrderDate(LocalDateTime orderDate) {
-		this.orderDate = orderDate;
-	}
-	public String getOrderStatus() {
-		return orderStatus;
-	}
-	public void setOrderStatus(String orderStatus) {
-		this.orderStatus = orderStatus;
-	}
-	public Customer getCustomer() {
-		return customer;
-	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
-	public Restaurant getRestaurant() {
-		return restaurant;
-	}
-	public void setRestaurant(Restaurant restaurant) {
-		this.restaurant = restaurant;
-	}
-	public DeliveryDriver getDeliveryDriver() {
-		return deliveryDriver;
-	}
-	public void setDeliveryDriver(DeliveryDriver deliveryDriver) {
-		this.deliveryDriver = deliveryDriver;
-	}
-    
-    
-    
-    
 
-   
+    @ManyToMany
+    @JoinTable(
+        name = "OrdersCoupons",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "coupon_id")
+    )
+    private Set<Coupons> coupons;
+    @JsonIgnore
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<OrderItem> orderItems;
+
+
+    public Integer getOrderId() { return orderId; }
+    public void setOrderId(Integer orderId) { this.orderId = orderId; }
+
+    public LocalDateTime getOrderDate() { return orderDate; }
+    public void setOrderDate(LocalDateTime orderDate) { this.orderDate = orderDate; }
+
+    public String getOrderStatus() { return orderStatus; }
+    public void setOrderStatus(String orderStatus) { this.orderStatus = orderStatus; }
+
+    public Customer getCustomer() { return customer; }
+    public void setCustomer(Customer customer) { this.customer = customer; }
+
+    public Restaurant getRestaurant() { return restaurant; }
+    public void setRestaurant(Restaurant restaurant) { this.restaurant = restaurant; }
+
+    public DeliveryDriver getDeliveryDriver() { return deliveryDriver; }
+    public void setDeliveryDriver(DeliveryDriver deliveryDriver) { this.deliveryDriver = deliveryDriver; }
+
+    public Set<Coupons> getCoupons() { return coupons; }
+    public void setCoupons(Set<Coupons> coupons) { this.coupons = coupons; }
+
+    public Set<OrderItem> getOrderItems() { return orderItems; }
+    public void setOrderItems(Set<OrderItem> orderItems) { this.orderItems = orderItems; }
 }
