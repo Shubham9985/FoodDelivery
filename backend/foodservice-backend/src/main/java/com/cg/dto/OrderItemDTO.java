4299687a81bedece4e532ca28c1f09df3d3fb9f8
Anima <animasingh78@gmail.com>
2026-04-28 14:36:15 +0530

    validation order

--- backend/foodservice-backend/src/main/java/com/cg/dto/OrderItemDTO.java
+++ backend/foodservice-backend/src/main/java/com/cg/dto/OrderItemDTO.java
@@ -1,9 +1,12 @@
 package com.cg.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Min;
+<<<<<<< HEAD
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+=======
+>>>>>>> bad4a5a (validation order)
 
 public class OrderItemDTO {
 
