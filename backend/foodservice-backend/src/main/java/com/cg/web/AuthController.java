788b5b3ce404054936d8d1af0113c0de6a30029b
Arpit Bisht <arpitbisht.31415@gmail.com>
2026-05-04 10:34:28 +0530

    changes in auth controller

--- backend/foodservice-backend/src/main/java/com/cg/web/AuthController.java
+++ backend/foodservice-backend/src/main/java/com/cg/web/AuthController.java
@@ -1,14 +1,15 @@
 package com.cg.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.dto.AuthRequest;
-import com.cg.dto.AuthResponse;
+import com.cg.dto.AuthResponseDTO;
+import com.cg.dto.LoginDTO;
 import com.cg.dto.RegisterDTO;
 import com.cg.service.AuthService;
 
@@ -20,16 +21,14 @@ public class AuthController {
 
     @Autowired
     private AuthService service;
-    
- // Login to Account
- 	@PostMapping("/login")
-     public AuthResponse login(@RequestBody AuthRequest request) {
-         String token = service.login(request.getEmail(), request.getPassword());
-         return new AuthResponse(token);
-     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@Valid @RequestBody RegisterDTO dto) {
-        return ResponseEntity.ok(service.register(dto));
+    public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody RegisterDTO dto) {
+        return new ResponseEntity<>(service.register(dto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginDTO dto) {
+        return ResponseEntity.ok(service.login(dto));
     }
 }
