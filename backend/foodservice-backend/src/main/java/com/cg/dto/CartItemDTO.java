87d609e4569f67d645e61c78f4487a3dff1fe56b
Anima <animasingh78@gmail.com>
2026-05-04 10:09:17 +0530

    updated dto

--- backend/foodservice-backend/src/main/java/com/cg/dto/CartItemDTO.java
+++ backend/foodservice-backend/src/main/java/com/cg/dto/CartItemDTO.java
@@ -1,8 +1,13 @@
 package com.cg.dto;
 
-public class CartItemDTO {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
+public class CartItemDTO {
+    @NotNull(message="Item ID is required")
 	private Integer itemId;
+    @NotNull(message="Quantity is required")
+    @Min(value=1,message="Quantity must be atleast 1")
 	private Integer quantity;
 	public Integer getItemId() {
 		return itemId;
